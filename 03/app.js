230adaa06180a382510357977b4b1dc00e274670
khk283855 <58452583+khk283855@users.noreply.github.com>
2020-04-13 20:07:29 +0900

    03 수업과제

---
+++ 03/app.js
@@ -0,0 +1,96 @@
+console.log('app');
+
+// create
+// var todos = ['운동'];
+// var todo = '게임';
+// todos.push(todo);
+// console.log(todos);
+
+// // read
+// var todos = ['운동', '게임'];
+// todos.forEach(function(todo) {
+//   console.log(todo);
+// });
+
+//update
+// var todos = ['운동', '게임'];
+// var updateTodo = '게임';
+// var updateIndex = todos.findIndex(function(todo) {
+//   return todo === updateTodo;
+// });
+// todos[updateIndex] = '공부';
+// console.log(todos);
+
+// //delete
+// var todos = ['운동', '공부', '목욕'];
+// var deleteTodo = '공부';
+// var deleteIndex = todos.findIndex(function(todo) {
+//   return todo === deleteTodo;
+// });
+// todos.splice(deleteIndex, 1);
+// console.log(todos);
+
+// var isMan = true;
+// if(isMan) {
+//     console.log('1');
+// }
+// else if(isMan && isHero) {
+
+// }
+//  else {
+//     console.log('2');
+// }
+
+// window.alert('1');
+// var who = prompt("당신은 누구세요?");
+// console.log(who);
+
+// var isDelete = confirm('정말 삭제합니까?');
+
+// console.log(isDelete);
+
+// function sum(a,b);{
+//     return a + b;
+// }
+// console.log(sum(1,2));
+
+// var car = {
+//     year: '2014',
+//     starting: function() {
+//       console.log('starting..');
+//     },
+//   };
+
+//   function sum(x, y) {
+//     if (!y) y = 1;
+//     console.log(x + y);
+//   }
+//   console.log(sum(3));
+//   console.log(sum(3,3));
+
+// var count = [10,20,30,40,50,60,70,100];
+// console.log((count[0]+count[1]+count[2]+count[3]+count[4]+count[5]+count[6]+count[7])/8)
+
+// 구구단
+// for(var i = 2; i < 10; i++){
+//     console.log(i+'단');
+//     for(var j = 1; j < 10; j++){
+//         console.log(i+'*'+j+'='+(i*j));
+//     }
+// }
+
+// function mul() {
+//     for(var i = 2; i < 10; i++){
+//         console.log(i+'단');
+//         for(var j = 1; j < 10; j++){
+//             console.log(i+'*'+j+'='+(i*j));
+//         }
+//     }
+// }
+// mul();
+// var isMan = {
+//     test : function(){
+//         console.log(this);
+//     }  
+// };
+// obj.test();
